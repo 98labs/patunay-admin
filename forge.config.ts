fe70e346b917416f774b544e669762457181c2ee
geoffrey <geoffrey.ayap@98labs.com>
2025-04-23 12:59:34 +0800

    update forge

--- forge.config.ts
+++ forge.config.ts
@@ -1,39 +1,35 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    icon: './src/ui/assets/logo/patunay-logo.png',
-    osxSign: {}
+    icon: './src/ui/assets/logo/patunay-256x256.png',
+    osxSign: {},
   },
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
       platforms: ['win32'],
       config: {
+        name: 'Patunay-app',
         authors: '98Labs Inc.',
-      }
+      },
     },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
       config: {
         authors: '98Labs Inc.',
-      }
+      },
     },
     {
       name: '@electron-forge/maker-deb',
       platforms: ['linux'],
       config: {
         authors: '98Labs Inc.',
-      }
+      },
     },
-    new MakerSquirrel({
-        name: 'Patunay-app',
-        authors: '98Labs Inc.',
-      }, ['win32']),
-  ]
+  ],
 };
 
 export default config;
