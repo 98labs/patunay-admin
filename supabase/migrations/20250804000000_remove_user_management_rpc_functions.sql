b7f1cb405170524fe39e03c334b546dcb2a6fd01
JohnHoneyman <johnhoneyman10@gmail.com>
2025-08-11 12:46:16 +0800

    Deleted migration scripts

--- supabase/migrations/20250804000000_remove_user_management_rpc_functions.sql
+++
@@ -1,8 +0,0 @@
--- Remove old RPC functions since we're using Edge Functions for user management
-DROP FUNCTION IF EXISTS public.create_user_with_profile CASCADE;
-DROP FUNCTION IF EXISTS public.update_user_profile CASCADE;
-DROP FUNCTION IF EXISTS public.soft_delete_user CASCADE;
-DROP FUNCTION IF EXISTS public.grant_user_permission CASCADE;
-DROP FUNCTION IF EXISTS public.revoke_user_permission CASCADE;
-DROP FUNCTION IF EXISTS public.get_user_with_role CASCADE;
-DROP FUNCTION IF EXISTS public.get_organization_users_secure CASCADE;
