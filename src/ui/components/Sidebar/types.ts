3f7776d897a11f3f379508ccf64bb29958f96476
JohnHoneyman <johnhoneyman10@gmail.com>
2025-08-14 11:56:48 +0800

    enhancement: Improve styling of sidebar

--- src/ui/components/Sidebar/types.ts
+++ src/ui/components/Sidebar/types.ts
@@ -1,15 +1,18 @@
+import { LucideIcon } from 'lucide-react';
+
 export interface Links {
-    name: string;
-    path: string;
-    children?: Links[];
-  }
-  
-  export interface NavbarItemProps {
-    currentPath: string;
-    name: string;
-    path: string;
-    childrenLinks?: Links[];
-    isChild?: boolean;
-    onNavigate?: () => void;
-  }
-  
+  name: string;
+  path: string;
+  icon?: LucideIcon;
+  children?: Links[];
+}
+
+export interface NavbarItemProps {
+  currentPath: string;
+  name: string;
+  path: string;
+  icon?: LucideIcon;
+  childrenLinks?: Links[];
+  isChild?: boolean;
+  onNavigate?: () => void;
+}
